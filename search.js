4da9f24d1ad4fa58895ec61943c64da35c2968a4
pomogranateis <02230295.cst@rub.edu.bt>
2024-05-05 23:15:41 +0600

    updated

---
+++ search.js
@@ -0,0 +1,30 @@
+const inputElement = document.querySelector("#search-input");
+const searchCloseIcon = document.querySelector("#search-close-icon");
+const sortWrapper = document.querySelector(".sort-wrapper");
+const filterWrapper = document.querySelector(".filter-wrapper");
+const body = document.querySelector("body");
+
+inputElement.addEventListener("input", () => {
+  handleInputChange(inputElement, searchCloseIcon);
+});
+searchCloseIcon.addEventListener("click", handleSearchCloseOnClick);
+sortWrapper.addEventListener("click", handleSortIconOnClick);
+
+function handleInputChange(inputElement, searchCloseIcon) {
+  const inputValue = inputElement.value;
+  if (inputValue !== "") {
+    searchCloseIcon.classList.add("search-close-icon-visible");
+  } else {
+    searchCloseIcon.classList.remove("search-close-icon-visible");
+  }
+}
+
+function handleSearchCloseOnClick() {
+  inputElement.value = "";
+  searchCloseIcon.classList.remove("search-close-icon-visible");
+}
+
+function handleSortIconOnClick() {
+  filterWrapper.classList.toggle("filter-wrapper-open");
+  body.classList.toggle("filter-wrapper-overlay");
+}
